de46ae28887fc169d6d7f8272522f7a5a3d23a2c
Eghosa Ordia <eghordia130@gmail.com>
2024-01-10 19:30:13 +0100

    attempt audio-download-fix

--- functions.py
+++ functions.py
@@ -2,10 +2,9 @@ from moviepy.editor import *
 
 
 def convert_video_to_audio(file_dir, file_ext, output_file_name):
-    video = VideoFileClip(file_dir+file_ext)
+    video = VideoFileClip(file_dir+'.'+file_ext)
     video.audio.write_audiofile(f"static/downloads/video/{output_file_name}.{file_ext}")
     return f'//static/downloads/audio/{output_file_name}'
 
 
 # print(convert_video_to_audio('static/downloads/video/Unstoppable', 'mp4', 'unstoppable'))
-# video = VideoFileClip('static/downloads/video/Chandelier(OfficialVideo).mp4')
