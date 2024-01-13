07a33364e6fa8e7310a9af9f557c0ac2939cc6be
Eghosa Ordia <eghordia130@gmail.com>
2024-01-13 22:11:47 +0100

    refactor functions

--- functions.py
+++ functions.py
@@ -2,15 +2,13 @@ from moviepy.editor import *
 
 
 def convert_video_to_audio(file_dir, file_ext, output_file_name, convert_type):
+    audio_format = '.mp3'
     if '.mp4' in output_file_name:
         output_file_name = output_file_name.replace('.mp4', '')
     elif '.mkv' in output_file_name:
         output_file_name = output_file_name.replace('.mkv', '')
     elif '.avi' in output_file_name:
         output_file_name = output_file_name.replace('.avi', '')
-    print(file_dir)
     video = VideoFileClip(file_dir)
-    video.audio.write_audiofile(f"static/downloads/audio/{output_file_name}.mp3")
-    return f'./static/downloads/audio/{output_file_name}.mp3'
-
-
+    video.audio.write_audiofile(f"static/downloads/audio/{output_file_name}{audio_format}")
+    return f'./static/downloads/audio/{output_file_name}{audio_format}'
